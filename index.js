a184920895824f5929bd1c7dea73085f9479699b
dgallegos <76798191+davidGallegos99@users.noreply.github.com>
2022-01-12 22:22:26 -0600

    change cors configuration

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization,  X-PINGOTHER'
     );
-    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
        
     next();
